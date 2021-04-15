37badddcab8de74f91ec5f1b9e5b1b69fa9ee067
natane <natane.baltezan@minutrade.com>
2021-04-15 15:46:39 -0300

    Refactor: update queries names

--- api/user/userResolvers.js
+++ api/user/userResolvers.js
@@ -14,8 +14,8 @@ const userResolvers = {
     parseLiteral: (ast) => new Date(ast.value)
   }),
   Query: {
-    users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
-    user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+    getUsers: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+    getUserById: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
   },
   Mutation: {
     addUser: async (root, { user }, { dataSources }) => dataSources.usersAPI.addUser(user),
